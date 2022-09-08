cd023cf90dfdba3b017aae51fb86964809a2799b
jatin singh <jatinsingh1998.gmail.com>
2022-09-08 10:18:06 +0530

    modify Login Page

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ function Validation() {
     
     var userInput = document.getElementById("mobilenumber").value;
     
-
-    if(userInput ===""){
-        document.getElementById("message").innerHTML="** Please Fill Mobile Number **";
-        return false;
-    };
     if(isNaN(userInput)){
         document.getElementById("message").innerHTML="** Enter Only Numeric Value **";
         return false;
     };
+    if(userInput ===""){
+        document.getElementById("message").innerHTML="** Please Fill Mobile Number **";
+        return false;
+    };
+
+  
     if(userInput.length<10){
         document.getElementById("message").innerHTML="** Mobile Number Must be 10 Digit **";
         return false;
@@ -21,6 +22,8 @@ function Validation() {
     }if((userInput.charAt(0)!=9) && (userInput.charAt(0)!=8) && (userInput.charAt(0)!=7)){
         document.getElementById("message").innerHTML="** Please Enter Valid Mobile Number **";
         return false;
+    }else{
+        document.getElementById("message").innerHTML="";
     }
 
     var passInput = document.getElementById("pass").value;
@@ -48,11 +51,23 @@ function Validation() {
         }if(passInput.search(/[A-Z]/)==-1){
             document.getElementById("password").innerHTML="** Password Of Atleast 1 Upper Case Charcter in the Box **";
             return false;
-        }if(passInput.search(/[!]/)==-1){
+        }if(passInput.search(/[!\@\#\$\%\^\&\*]/)==-1){
             document.getElementById("password").innerHTML="** Password Of Atleast 1 Special Charcter in the Box **";
             return false;
+        }else{
+            document.getElementById("password").innerHTML="";
         }
       
 
  }
+
+ function myFunction() {
+    var showPassword = document.getElementById("pass");
+    if (showPassword.type === "password") {
+        showPassword.type = "text";
+    } else {
+        showPassword.type = "password";
+    }
+  }
+
  
